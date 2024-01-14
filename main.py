b6c84925ab7ffe8ba4070c2598c3e9369985c801
George Papeorgiou <george@Dias.home>
2024-01-14 21:49:18 +0200

    Added Expression and Math Class

--- main.py
+++ main.py
@@ -1,20 +1,70 @@
 from sympy import *
+import time
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 class Expression:
-    def __init__(self, expr):
+    def __init__(self, string):
         self.x = Symbol('x')
-        self.expr = expr
-        self.sexpr = sympify(self.expr)
+        self.string = string
+        self.sexpr = sympify(self.string)
     
     def eval(self, point):
-        print(self.sexpr.evalf(subs={self.x:point}))
+        return self.sexpr.evalf(subs={self.x:point})
 
+
+class Math:
+    def __init__(self, exprobj, h, start, end):
+        self.exprobj = exprobj
+        self.h = h
+        self.start = start
+        self.end = end
+        self.val, self.axis = self.__createValues()
+
+    def fft(self):
+        fastFourier = np.fft.fft(self.val)
+        return np.abs(fastFourier)
+
+    def __createValues(self):
+        val = np.array([])
+        axis = np.array([])
+        #val = []
+        #axis = []
+
+        k = self.start 
+        v = int((abs(self.start - self.end) / self.h))
+        print(v)
+
+        for i in range(v):
+            #val.append(self.exprobj.eval(k))
+            val = np.append(val, self.exprobj.eval(k))
+            k += self.h
+            axis = np.append(axis, k)
+            #axis.append(k)
+
+        return val, axis
 
 
 def main():
-    string = 'x**2' 
+    start = time.time()
+    string = 'sin(2*pi*x)' 
     e = Expression(string)
+    
+    m = Math(e, 0.1, -2*pi, 2*pi) 
+    end = time.time()
+    #plt.plot(m.axis, m.val)
+    #plt.show()
+    
+    f = m.fft() 
+    a = np.linspace(-62,62,num = 125)
+    plt.plot(a, f)
+    plt.show()
+
+    print(a)
+
+
+
 
 if __name__ == "__main__":
     main()
